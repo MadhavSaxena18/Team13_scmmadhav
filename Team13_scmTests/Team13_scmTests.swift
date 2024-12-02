6edc5edb4ad5dab3305988633b1993b604e0227c
shashvat <singhshashvat4@gamil.com>
2024-12-02 12:21:21 +0530

    Initial Commit

---
+++ Team13_scmTests/Team13_scmTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team13_scmTests.swift
+//  Team13_scmTests
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import Testing
+@testable import Team13_scm
+
+struct Team13_scmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
